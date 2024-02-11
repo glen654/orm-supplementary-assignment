2303eaab321633894e73eaebee27100a22fc135d
Glen Alloy <glenalloy8@gmail.com>
2024-02-11 13:01:01 +0530

    few changes

--- src/main/java/lk/ijse/Main.java
+++ src/main/java/lk/ijse/Main.java
@@ -1,14 +1,19 @@
 package lk.ijse;
 
 import lk.ijse.config.FactoryConfiguration;
+import lk.ijse.entity.Author;
+import lk.ijse.entity.Book;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         Session session = FactoryConfiguration.getFactoryConfiguration().getSession();
         Transaction transaction = session.beginTransaction();
-
 
         transaction.commit();
         session.close();
