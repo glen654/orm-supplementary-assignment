24d382f2b0946b657eecc29d158e27f7261bebf7
Glen Alloy <glenalloy8@gmail.com>
2024-02-12 12:23:42 +0530

    few changes

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-ORM Supplementary Assignment
+# **ORM(Object-Relational Mapping) Supplementary Assignment**
+
+### **@Entity Annotation**
+
+This annotation, part of the javax. persistence package and is used at the class level and marks the class as a persistence entity.It signals to the JPA provider that this class should be treated as a table in the database.
+
+### **@Id Annotation**
+
+The @Id annotation is inherited from javax. persistence.id, Indicating that the member-field below is the primary key of the current entity.
+
+### **@GeneratedValue Annotation**
+
+The @GeneratedValue annotation is used to specify how the primary key values of an entity are generated.
+
+### **@OneToMany Annotation**
+
+@OneToMany annotation in Hibernate is used to obtain one-to-many relationships between two entities. It is used to map a collected-valued association where a single instance of an entity is mapped to multiple instances of another entity.
+
+### **@ManyToOne Annotation**
+
+@ManyToOne annotation in Hibernate is used to create a many-to-one relationship between two entities. The @ManyToOne annotation indicates that the many instances of one entity can be associated with only one instance of another entity.
+
+### **@JoinColumn Annotation**
+
+The @JoinColumn annotation in Hibernate is used to specify the mapping of a foreign key column in a relationship between two entities. The @JoinColumn annotation is applied on the owning side of the association to define the foreign key column name and other attributes which are related to the join column.
+
+
+
+
--- src/main/java/lk/ijse/Main.java
+++ src/main/java/lk/ijse/Main.java
@@ -9,11 +9,53 @@ import org.hibernate.query.Query;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
         Session session = FactoryConfiguration.getFactoryConfiguration().getSession();
         Transaction transaction = session.beginTransaction();
+
+       /* Query query = session.createQuery("select title from Book where publicationYear > '2010'");
+        List<String> titles = query.list();
+        for(String title : titles){
+            System.out.println(title);
+        }*/
+
+        /*Query query = session.createQuery("update Book set price = price + (price * 10.0 / 100.0)  where author.id = :author_id");
+        query.setParameter("author_id",1);
+        int count = query.executeUpdate();
+        System.out.println(count);*/
+
+       /* Query query = session.createQuery("select avg (price) from Book ");
+        Object result = query.uniqueResult();
+        System.out.println(result);*/
+
+       /* Query query = session.createQuery("select a.name, count(b.title) from Author a join Book b on a.id = b.author.id where a.id = :author_id");
+        query.setParameter("author_id",2);
+        List<Objects[]> results = query.list();
+        for (Object[] result : results) {
+            String authorName = (String) result[0];
+            Long bookCount = (Long) result[1];
+            System.out.println("Author: " + authorName + ", Book Count: " + bookCount);
+        }*/
+
+        /*Query query = session.createQuery("select a, count(b.title) from Author a left join a.books b where a.id = :author_id group by a.id");
+        query.setParameter("author_id",2);
+        List<Object[]> results = query.list();
+
+        for(Object[] result : results){
+            Author author = (Author) result[0];
+            Long book = (Long) result[1];
+
+            System.out.println("Author: " + author.getName() + ",Book: " + book);
+        }*/
+       /* Query query = session.createQuery("select a.name from Author a where (select count(b.title) from Book b where b.author = a) > " +
+                "(select avg(count(b.title)) from Book b group by b.author)");
+        List<String> authors = query.list();
+        for(String authorName : authors){
+            System.out.println(authorName);
+        }*/
 
         transaction.commit();
         session.close();
