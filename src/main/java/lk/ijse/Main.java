41f928bf964fbb973f10d11221dff7efdc2a7f41
Glen Alloy <glenalloy8@gmail.com>
2024-02-15 12:34:01 +0530

    few changes

--- src/main/java/lk/ijse/Main.java
+++ src/main/java/lk/ijse/Main.java
@@ -17,41 +17,49 @@ public class Main {
         Session session = FactoryConfiguration.getFactoryConfiguration().getSession();
         Transaction transaction = session.beginTransaction();
 
-        /*Query query = session.createQuery("select title from Book where publicationYear > :year");
-        query.setParameter("year",2010);
-        List<String> titles = query.list();
+       /* Write an HQL query to retrieve all books published after the year 2010.*/
+        Query query1 = session.createQuery("select title from Book where publicationYear > :year");
+        query1.setParameter("year",2010);
+        List<String> titles = query1.list();
         for(String title : titles){
             System.out.println(title);
-        }*/
+        }
 
-        /*Query query = session.createQuery("update Book set price = price + (price * 10.0 / 100.0)  where author.id = :author_id");
-        query.setParameter("author_id",1);
-        int count = query.executeUpdate();
-        System.out.println(count);*/
+       /* Write an HQL update query to increase the price of all books published by a specific
+        author by 10%.*/
+        Query query2 = session.createQuery("update Book set price = price + (price * 10.0 / 100.0)  where author.id = :author_id");
+        query2.setParameter("author_id",1);
+        int count = query2.executeUpdate();
+        System.out.println(count);
 
+        /*Write an HQL query to find the average price of all books.*/
+        Query query3 = session.createQuery("select avg (price) from Book ");
+        Object result = query3.uniqueResult();
+        System.out.println(result);
 
-        /*Query query = session.createQuery("select avg (price) from Book ");
-        Object result = query.uniqueResult();
-        System.out.println(result);*/
+        /*Write an HQL query to retrieve all authors along with the count of books they have written.*/
+        Query query4 = session.createQuery("select a.name, count (b.title) from Author a left join a.books b group by a.id,a.name");
+        List<Object[]> bookCount = query4.list();
 
-        /*Query query = session.createQuery("select a.name, count (b.title) from Author a left join a.books b group by a.id,a.name");
-        List<Object[]> bookCount = query.list();
+        for(Object[] results : bookCount){
+            System.out.println("Author: " + results[0] + ", Count: " + results[1]);
+        }
 
-        for(Object[] result : bookCount){
-            System.out.println("Author: " + result[0] + ", Count: " + result[1]);
-        }*/
-
-        Query query = session.createQuery("select b from Book b join b.author a where a.country = :countryName");
-        query.setParameter("countryName","Sri Lanka");
-        List<Book> bookList = query.list();
+        /*Write an HQL query using named parameters to retrieve books written by authors from a
+        specific country.*/
+        Query query5 = session.createQuery("select b from Book b join b.author a where a.country = :countryName");
+        query5.setParameter("countryName","Sri Lanka");
+        List<Book> bookList = query5.list();
         System.out.println(bookList);
 
-       /* Query query = session.createQuery("select a.name from Author a where (select count(b.title) from Book b where b.author = a) > " +
+        Query query6 = session.createQuery("select a from Author a where (select count (b) from Book b where b.author = a )");
+
+        Query query7 = session.createQuery("select a.name from Author a where (select count(b.title) from Book b where b.author = a) > " +
                 "(select avg(count(b.title)) from Book b group by b.author)");
-        List<String> authors = query.list();
+        List<String> authors = query7.list();
         for(String authorName : authors){
             System.out.println(authorName);
-        }*/
+        }
 
         transaction.commit();
         session.close();
